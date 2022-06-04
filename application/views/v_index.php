7aacd4d81b05d526804830d01ea0b468e61964a1
jhujhu <junartijk@gmail.com>
2022-06-04 23:00:56 +0700

    menambahkan controller, footer, header,index

---
+++ application/views/v_index.php
@@ -0,0 +1,41 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <p align='justify'> pada pengertian codeigniter diatas tadi di jelaskan bahwa codeigniter menggunakan metode MVC.
+         Apa itu 45 MVC? kita juga mengetahui apa itu MVC sebelum masuk dan lebih jauh dalam belajar codeigniter.</p>
+         <p>MVC adalah teknik atau konsep yang memisahkan komponen utama menjadi tiga komponem yaitu model, view dan controller.</p>
+         < type="a">
+             <li>model</li>
+             <p align='justify'> model adalah kelas yeng mempresentasikan atau 
+                memodelkan tipe data yang akan digunakanoleh aplikasi. model juga
+                dapat didefinisikan sebagai bagian penanganan yang berhubungan dengan
+                pengolahan atau manipulasi database. seperti misalnya mengambil data 
+                dari database, menginput dan pengolahan database lainnya. sem_acquireintriksi atau fungsi yang berhubungan dengan pengolahan database di
+                letakan di dalam model. sebagai contoh, jika ing9in membuat aplikasi
+                untuk menghitung luas dan keliling lingkaran, makan dapat 
+                memodelkan objek lingkaran sebagai kelas model.</p>
+                <p align='justify'> sebagai catatan, semua model harus disimpan didala folder application\models</p>
+                <p align='justify'>view merupakan bagian yang menangani halaman user
+                    interface atau halaman yang muncul pada user (pada browser). tampilan
+                dari user interface dikumpulkan pada view untuk memisahkannya
+            dengan controller dan model sehingga memudahkan web designer dala melakukan pengembagan 
+        tampilan halaman website.</p>
+        <li>controller</li>
+        <p align='justify'> contoller merupakan kumpulan intruksi aksi yang
+            meghbungkan model dengan view, jadi user tidak akan berhubungan 
+            dengan model secara langsung, intinya data yang tersimpan di
+            database (model) diambil oleh controller dan kemudian controller 
+            pula yang manampilkan nya e view. jadi controller lah yang mengolah intruksi.</p>
+            <p align='justify'> dari penjelasan tentang model view dan controller
+                di atas dapat disimpulkan  bahwa controller sebagai penghubing view
+                dan model. misalnya pada aplikasi yang menampilkan data dengan 
+                menggubakan metode konsep mvc, controller memanggil intruksi pada
+                model yang mengambil data pada database, keudian controller yang
+                meneruskan pada view untuk ditampilkan. jadi jelas sudah dan
+                sangat mudah dalam pengembangan aplikasi dengan cara mvc ini karena 
+                 web designer atau front-end developer tidak perlu lagi berhubungan 
+                 dengan controller, dia hanya perlu berhubungan dengan view untuk 
+                 mendesign tampilan aplikasi, karena back-end developer yang menanganinagian controller
+                 dan modelnya. jadi pembagian tugas pun menjadi mudah dan pengembangan aplikasi dapat 
+                 dilakukan dengan cepat dam tersetruktur.</p>
+         
+</section>
